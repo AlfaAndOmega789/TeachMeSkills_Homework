b73d17d40d6add7aaf27d9c2f4c13c01d72d48f9
Ivan <alfaandomega789@gmail.com>
2023-08-28 21:14:31 +0300

    add task 7 and add some changes the task 6

--- homework_2/Test.java
+++ homework_2/Test.java
@@ -1,16 +1,25 @@
 package homework_2;
 
 
+import java.util.Scanner;
+
 import static java.lang.Character.getNumericValue;
 
 public class Test {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(firstTask(7.7,7.0)); //task 1
         System.out.println(secondTask(25)); // task 2
         System.out.println(thirdTask(128)); //task 3
         System.out.println(fourthTask(5.5424234234234141)); //task 4
         System.out.println(fifthTask(21, 8)); //task 5
         System.out.println(sixthTask(1, 2)); //task 6
+
+        System.out.println("Результат сложения: " + seventhTaskAddition(sc.nextInt(), sc.nextInt())); //task 7.1
+        System.out.println("Результат вычитания: " + seventhTaskSubtraction(sc.nextInt(), sc.nextInt())); // task 7.2
+        System.out.println("Результат умножения: " + seventhTaskMultiplication(sc.nextInt(), sc.nextInt())); //task 7.3
+        System.out.println("Результат деления: " + seventhTaskDivision(sc.nextInt(), sc.nextInt())); //task 7.4
     }
 
     public static double firstTask(double b, double c){
@@ -43,6 +52,27 @@ public class Test {
     }
 
     public static String sixthTask(int a, int b) {
-        return b + "" + a;
+        int c = 0;
+        c = a;
+        a = b;
+        b = c;
+
+        return a + " " + b;
     }
+
+    public static double seventhTaskAddition(double a, double b){
+        return a + b;
+    }
+    public static double seventhTaskSubtraction(double a, double b){
+        return a  - b;
+    }
+    public static double seventhTaskMultiplication(double a, double b){
+        return a * b;
+    }
+
+    public static double seventhTaskDivision(double a, double b){
+        return (double)a / b + (double) a % b;
+    }
+
+
 }
