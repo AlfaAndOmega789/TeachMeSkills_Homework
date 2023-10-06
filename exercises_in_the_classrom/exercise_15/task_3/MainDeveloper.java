00cac712877e2e85f6bc7810c9b7201181b8754d
Ivan <alfaandomega789@gmail.com>
2023-10-06 14:38:32 +0300

    change exercise 15, task 3

--- exercises_in_the_classrom/exercise_15/task_3/MainDeveloper.java
+++ exercises_in_the_classrom/exercise_15/task_3/MainDeveloper.java
@@ -7,11 +7,13 @@ import java.util.stream.Stream;
 public class MainDeveloper {
     public static void main(String[] args) {
         List<Developer> list = new ArrayList();
-        list.add(new Developer(1, "Ivan"));
-        list.add(new Developer(2, "Anton"));
-        list.add(new Developer(3, "Petya"));
+        list.add(new Developer(10, "Ivan"));
+        list.add(new Developer(20, "Anton"));
+        list.add(new Developer(30, "Petya"));
 
         list.stream().filter(x -> x.getId() > 10 &&
-                x.getName().substring(0,1).equals("An")).forEach(System.out :: println);
+                x.getName().substring(0,2).equals("An")).forEach(System.out :: println);
+
+//        System.out.println(list.get(1).getName().substring(0,2));
     }
 }
