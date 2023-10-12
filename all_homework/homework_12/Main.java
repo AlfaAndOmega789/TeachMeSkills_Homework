dab6aaf037980e8508189e45fb4daf8f9d71dad8
Ivan <alfaandomega789@gmail.com>
2023-10-12 10:45:49 +0300

    change exercise 12

--- all_homework/homework_12/Main.java
+++ all_homework/homework_12/Main.java
@@ -3,32 +3,55 @@ package all_homework.homework_12;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(
-                "C:\\Projects\\TeachMeSkills_Homework\\all_homework\\homework_12\\Romeo and Juliet.txt"));
+        List<String> list = new ArrayList<>();
 
-        String stroka = reader.readLine();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(
+                    "C:\\Projects\\TeachMeSkills_Homework\\all_homework\\homework_12\\romeo-and-juliet.txt"));
+
+            String line = reader.readLine();
+            while (line != null) {
+
+                String[] array = line.split(" ");
+
+                int maxLength = Integer.MIN_VALUE;
+                int numberWorld = Integer.MIN_VALUE;
+
+                for(int i = 0; i < array.length;i++){
+                    if(maxLength < array[i].length()){
+                        maxLength = array[i].length();
+                        list.add(array[i]);
+                    }
+                }
 
 
-        String[] array = stroka.split(" ");
-
-        int maxLength = Integer.MIN_VALUE;
-        int numberWorld = Integer.MIN_VALUE;
-
-        for(int i = 0; i < array.length;i++){
-            if(maxLength < array[i].length()){
-                maxLength = array[i].length();
-                numberWorld = i;
+                line = reader.readLine();
             }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int count = 0;
+        int countWord = 0;
+        int lengthWord = 0;
+
+        for(String str : list){
+            if(lengthWord < str.length()){
+                lengthWord = str.length();
+                countWord = count;
+            }
+            count++;
         }
 
         BufferedWriter writer = new BufferedWriter(new FileWriter(
                 "C:\\Projects\\TeachMeSkills_Homework\\all_homework\\homework_12\\otherFile.txt"));
-        writer.write(array[numberWorld]);
+        writer.write(list.get(countWord));
 
-        reader.close();
         writer.close();
     }
 
---
+++ all_homework/homework_15/task_1/MainWorkWithStream.java
@@ -0,0 +1,21 @@
+package all_homework.homework_15.task_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainWorkWithStream {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(300);
+        list.add(400);
+        list.add(500);
+
+//        list.stream().filter(x -> {
+//            if(x.equals(x.)  ){
+//
+//            }
+//        });
+    }
+}
