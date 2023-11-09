2ba6ecf63ea9c474727548da5ce2b75a38f3bb6d
Ivan <alfaandomega789@gmail.com>
2023-11-09 18:23:12 +0300

    init commit

--- all_homework/homework_18/task_1/Main.java
+++ all_homework/homework_18/task_1/Main.java
@@ -1,12 +1,10 @@
-package exercises_in_the_classrom.exercise_18.task_1;
+package all_homework.homework_18.task_1;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedWriter;
@@ -19,9 +17,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-
-
-        Sonnet sonnet = new Sonnet();
 
         Document doc;
         try {
